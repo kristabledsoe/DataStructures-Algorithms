9a6833f160ae836da7b0f2861c9fdc63bb5e1693
kristabledsoe <krista.m.bledsoe@gmail.com>
2021-12-13 19:02:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ejp7nm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ejp7nm)
---
+++ index.js
@@ -0,0 +1,25 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const coins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+determineWinners(coins);
+
+function determineWinners(coins) {
+  for (let i = 0; i < coins.length; i++) {
+    const winner = determineWinner(coins[i]);
+    console.log(`Nbr of Coins: ${coins[i]} and winner is: ${winner}`);
+  }
+}
+
+function determineWinner(nbrCoins) {
+  if ((nbrCoins - 1) % 3 === 0) {
+    return 'them';
+  } else {
+    return 'you';
+  }
+}
