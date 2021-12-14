15fbea3cf9d4b14855aea4bf93329ae27f2b1885
kristabledsoe <krista.m.bledsoe@gmail.com>
2021-12-13 20:32:18 -0500

    1 file modified

--- index.js
+++ index.js
@@ -5,24 +5,51 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
 
-// The "Coin Game" - START
-const coins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // The "Coin Game" - START
+// const coins = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-determineWinners(coins);
+// determineWinners(coins);
 
-function determineWinners(coins) {
-  for (let i = 0; i < coins.length; i++) {
-    const winner = determineWinner(coins[i]);
-    console.log(`Nbr of Coins: ${coins[i]} and winner is: ${winner}`);
+// function determineWinners(coins) {
+//   for (let i = 0; i < coins.length; i++) {
+//     const winner = determineWinner(coins[i]);
+//     console.log(`Nbr of Coins: ${coins[i]} and winner is: ${winner}`);
+//   }
+// }
+
+// function determineWinner(nbrCoins) {
+//   if ((nbrCoins - 1) % 3 === 0) {
+//     return 'them';
+//   } else {
+//     return 'you';
+//   }
+// }
+
+// // The "Coin Game" - END
+
+// TODO: Add in the "Sum Swap"
+
+// GREEDY Algorithms
+
+// Highest Nbr in Array - START
+
+const myArray = [4,7,8,2,3,9,5,4,1];
+
+const result = getHighestNumber(myArray);
+console.log('result: ', result);
+
+function getHighestNumber(myArray) {
+  let highestNumber = 0;
+
+  for (let i = 0; i < myArray.length; i++) {
+    if (myArray[i] > highestNumber) {
+      highestNumber = myArray[i];
+    }
   }
+
+  return highestNumber;
 }
 
-function determineWinner(nbrCoins) {
-  if ((nbrCoins - 1) % 3 === 0) {
-    return 'them';
-  } else {
-    return 'you';
-  }
-}
+// Highest Nbr in Array - END
 
-// The "Coin Game" - END
+
